193587dba437a740b093c589db2bfdef66fb663f
Luna Barber <lunabbarber@gmail.com>
2026-04-08 22:57:52 -0600

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import prettier from "eslint-config-prettier";
 
 // Prettier config always has to be last
+// This is useful for CI/CD (maybe for github actions)
 
 /** @type {import('eslint).Linter.Config[]} */
 export default [
